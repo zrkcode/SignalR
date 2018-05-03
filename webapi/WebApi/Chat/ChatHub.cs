0eb1a2a1ed33409f7914f6a2018c72655c3051bf
MrGuoKe <395183997@qq.com>
2018-05-03 10:59:14 +0800

    Asp.NetCore+Microsoft.AspNetCore.SignalR前后端分离

---
+++ webapi/WebApi/Chat/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebApi.Chat
+{
+    public class ChatHub : Hub
+    {
+        //SendMsg用于前端调用
+        public Task SendMsg(ChatMessageInfo info)
+        {
+            //在客户端实现此处的Show方法
+            return Clients.All.SendAsync("Show", info.UserName + ":" + info.Message);
+        }
+    }
+}
---
+++ webapi/WebApi/Chat/ChatMessageInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Chat
+{
+    public class ChatMessageInfo
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
